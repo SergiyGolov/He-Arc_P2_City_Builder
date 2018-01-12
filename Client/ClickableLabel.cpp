c7220839999883db5b496b53a5c627bc0528097f
Raphael Margueron <raphael.margueron@he-arc.ch>
2018-01-12 22:47:11 +0100

    adding save features, and adapating services class the have the possibilities to get the variable + adding top hud features => like tracking the time and changing the city's name

---
+++ Client/ClickableLabel.cpp
@@ -0,0 +1,6 @@
+#include "ClickableLabel.h"
+
+ClickableLabel::ClickableLabel()
+{
+
+}
---
+++ Client/ClickableLabel.h
@@ -0,0 +1,12 @@
+#ifndef CLICKABLELABEL_H
+#define CLICKABLELABEL_H
+
+#include <QWidget>
+
+class ClickableLabel : public QLabel
+{
+public:
+    ClickableLabel();
+};
+
+#endif // CLICKABLELABEL_H
